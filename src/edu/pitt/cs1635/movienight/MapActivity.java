8f806a222f63f0ed61a053a47e7ab4d126098384
Cory Savit <cory.savit@gmail.com>
2014-03-27 00:08:09 -0400

    maps focuses on location

--- src/edu/pitt/cs1635/movienight/MapActivity.java
+++ src/edu/pitt/cs1635/movienight/MapActivity.java
@@ -1,6 +1,7 @@
 package edu.pitt.cs1635.movienight;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,10 +9,17 @@ import android.view.Menu;
 
 
 public class MapActivity extends Activity {
+	
+	private GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        // Getting GoogleMap object from the fragment
+
+        // Enabling MyLocation Layer of Google Map
+        map.setMyLocationEnabled(true);
     }
     
     @Override
