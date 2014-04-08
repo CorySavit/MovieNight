9339a4c9cff889aaf66798899ca4157471bc7516
Cory Savit <cory.savit@gmail.com>
2014-04-08 14:39:31 -0400

    Added ProfileEdit

--- src/edu/pitt/cs1635/movienight/ProfileEdit.java
+++ src/edu/pitt/cs1635/movienight/ProfileEdit.java
@@ -6,30 +6,25 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.AdapterView.OnItemClickListener;
 
-public class Profile extends Activity {
+public class ProfileEdit extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.profile_main);
+		setContentView(R.layout.activity_profile_edit);
 		
-		Button editButton = (Button) findViewById(R.id.edit_profile_btn);
+		Button editButton = (Button) findViewById(R.id.save_profile_btn);
 		editButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(getApplicationContext(), ProfileEdit.class);
-				startActivity(intent);
+				finish();
 				
 			}
 			
 		});
-		
 	}
 
 	@Override
